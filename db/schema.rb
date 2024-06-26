fdcdbdf246dd213e590a86f446b8d5ded825972c
saintDW <chatnapat.s@datawow.io>
2024-06-26 13:32:55 +0700

    find by id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_26_043825) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_26_044019) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,4 +22,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_26_043825) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "comment"
+    t.integer "star"
+    t.bigint "books_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["books_id"], name: "index_reviews_on_books_id"
+  end
+
+  add_foreign_key "reviews", "books", column: "books_id"
 end
