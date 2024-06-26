a13452a70c527b6f32769f58c3e9fe126ed12349
saintDW <chatnapat.s@datawow.io>
2024-06-26 15:36:10 +0700

    api update, delete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,9 +21,30 @@ class BooksController < ApplicationController
     end
   end
 
+  def edit
+    @books = Book.find(params[:id])
+  end
+
+  def update
+    @books = Book.find(params[:id])
+
+    if @books.update(book_params)
+      redirect_to @books
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @books = Book.find(params[:id])
+    @book.destroy
+
+    redirect_to root_path
+  end
+
   private
 
   def book_params
-    params.require(:books).permit(:name, :description)
+    params.require(:books).permit(:name, :description, :release)
   end
 end
