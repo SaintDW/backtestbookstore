88f9b089982d21e2aed52900dd1ae38ea10bcb63
saintDW <chatnapat.s@datawow.io>
2024-06-27 18:10:16 +0700

    add api get post

--- app/api/v1/api.rb
+++ app/api/v1/api.rb
@@ -4,8 +4,34 @@ module V1
   class Api < Grape::API
     format :json
     resource :books do
+      desc 'Get all'
       get do
         Book.all
+      end
+
+      desc 'Get by id'
+      params do
+        requires :id, type: Integer, desc: 'Book ID'
+      end
+      route_param :id do
+        get do
+          Book.find(params[:id])
+        end
+      end
+
+      desc 'Create Book'
+      params do
+        requires :name, type: String, desc: 'Book Name'
+        requires :description, type: String, desc: 'Book Description'
+        requires :release, type: DateTime, desc: 'Book Release'
+      end
+      post do
+        user = Book.create({
+                             name: params[:name],
+                             description: params[:description],
+                              release: params[:release]
+                           })
+        present user
       end
     end
   end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :release, presence: true
 end
