6ad12fb6523313bdc4f9d50ac4032addb7740130
saintDW <chatnapat.s@datawow.io>
2024-07-02 15:39:32 +0700

    add radis in api get_all_book, get_book_by_id

--- app/services/book_service.rb
+++ app/services/book_service.rb
@@ -1,10 +1,28 @@
 class BookService
   def self.get_all_book
-    Book.all
+    cached_book = Rails.cache.read(Book.first.cache_key_with_version)
+    if cached_book
+      puts 'cached_book from cached'
+      return cached_book
+    end
+
+    puts 'cached_book from DB'
+    Rails.cache.write(Book.first.cache_key_with_version, Book.all)
+    Rails.cache.read(Book.first.cache_key_with_version)
   end
 
   def self.get_book_by_id(book_id)
-    Book.find(book_id)
+    cached_book = Rails.cache.read(Book.first.cache_key_with_version)
+    if cached_book
+      puts 'cached_bookid from cached'
+      cached_book_id = cached_book.find { book_id }
+      return cached_book_id
+    end
+
+    puts 'cached_book_id from DB'
+    Rails.cache.write(Book.first.cache_key_with_version, Book.all)
+    cached_book_id = Rails.cache.read(Book.first.cache_key_with_version)
+    cached_book.find { cached_book_id }
   end
 
   def self.create_book(name, description, release)
